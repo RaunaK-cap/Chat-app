25cc667d53708a8475aa4296539c0ff5efde8c3a
Raunak <raunakkumar6437@gmail.com>
2025-04-17 15:16:35 +0530

    improved UI

--- 03-FE-websocket/src/pages/Homepage.tsx
+++ 03-FE-websocket/src/pages/Homepage.tsx
@@ -9,7 +9,7 @@ interface schema{
 
 const Homepage = ({roomid , setroomid , username , setusername , handler}: schema) => { 
   return (
-    <div className={`h-screen text-white bg-linear-7 from-black to-gray-300 `}>
+    <div className={`h-screen text-white bg-black bg-cover `}>
       
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
       <h1 className=' font-bold text-5xl'>Chat Here ...</h1>
@@ -17,21 +17,22 @@ const Homepage = ({roomid , setroomid , username , setusername , handler}: schem
       {/* <button className='bg-black , text-white ' onClick={()=>setcolor(!color)}>{ `${color? "white" : "dark"}`}</button> */}
       <div className="py-5">
         <input
+        
         value={roomid}
         onChange={(e)=> {setroomid(e.target.value)}}
-        className=' p-2 rounded-lg font-mono  text-black bg-white'
+        className=' p-2 rounded-xl font-mono  outline-none text-black bg-white'
         placeholder='Enter the any roomid '/>
       </div>
 
       <input 
       value={username} 
         onChange={(e)=>{setusername(e.target.value)}} 
-        className='p-2 rounded-lg text-black bg-white font-mono '
+        className='p-2 rounded-xl text-black bg-white font-mono outline-none '
         placeholder='Enter your name '/>
       <div>
 
       </div>
-      <button onClick={handler} className="p-2 bg-white text-black rounded-xl hover:cursor-pointer gap-1 px-3 mx-20 my-8 font-medium font-mono "> Join </button>
+      <button onClick={handler} className="p-2 bg-black text-white border border-zinc-300 rounded-xl hover:cursor-pointer gap-1 px-3 mx-20 my-8 font-medium font-mono "> Join </button>
       
     </div>
      
