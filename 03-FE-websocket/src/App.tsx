3c4db22f65ef251883a0439dead6f80808b60d72
Raunak <raunakkumar6437@gmail.com>
2025-04-18 00:34:55 +0530

    fix receving data

--- 03-FE-websocket/src/App.tsx
+++ 03-FE-websocket/src/App.tsx
@@ -1,24 +1,39 @@
 
 import Homepage from './pages/Homepage'
 import Pages from './pages/Chatpage'
-import { Route  ,  Routes, useNavigate } from 'react-router'
+import { data, Route  ,  Routes, useNavigate } from 'react-router'
 
 
 import './App.css'
 import { useEffect, useState } from 'react'
+
+interface chatmsg{
+  text:string,
+  isuser:boolean
+}
 
 function App() {
 
       const [roomid, setroomid] = useState<string>("")
       const[username , setusername] = useState<string>("")
       const navigate = useNavigate()
-      const[socket , setsocket] = useState<WebSocket>()
+      const[socket , setsocket] = useState<WebSocket|undefined>()
       
-      const[chatmsg, setchatmsg] = useState([])
+      const[chatmsg, setchatmsg] = useState<chatmsg[]>([])
 
       function handler(){
         if(roomid && username){    
-          navigate('/chat')
+          const datatosend = JSON.stringify({
+            "type": "join",
+            "payload": {
+              "roomid": roomid,
+              "username":username
+            }
+         });
+
+        socket.send(datatosend)
+
+         navigate('/chat')
         }else{
           alert("Enter all Information..")
         }
@@ -28,22 +43,18 @@ function App() {
         setsocket(ws)
         ws.onopen = ()=>{
           console.log("connected baby")
-          
         }  
         }, [])
         
 
   return (
     <>
-    
     <Routes>
-      <Route path='/' element = {<Homepage roomid= {roomid} setroomid={setroomid} username={username} setusername={setusername} handler={handler} />}/>
-
-      <Route path='/chat' element={<Pages roomid={roomid} username={username} socket={socket} chatmsg={chatmsg} setchatmsg ={setchatmsg}/>}/>
-
+      <Route path='/' element = {<Homepage roomid= {roomid} setroomid={setroomid} username={username} setusername={setusername} handler={handler} socket={socket}  />}/>
+      <Route path='/chat' element={<Pages roomid={roomid} username={username} 
+      //@ts-ignore
+      socket={socket} chatmsg={chatmsg} setchatmsg ={setchatmsg}/>}/>
     </Routes>
-    
-
 
     </>
   )
