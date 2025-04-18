1ceb03c7899902c042a91a3cc244fc220afd5e41
Raunak <raunakkumar6437@gmail.com>
2025-04-19 01:02:41 +0530

    added loader page

---
+++ 03-FE-websocket/src/pages/Loader.tsx
@@ -0,0 +1,15 @@
+
+
+const Loader = () => {
+  return (
+    <div className=' h-screen w-full text-white bg-black'>
+        <div className=" relative top-1/2 left-1/2 -translate-y-[50%] -translate-x-[50%]   ">
+            <div className=" flex justify-center items-center">
+            <p className="font-mono font-semibold text-4xl"> Loading ...</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Loader
